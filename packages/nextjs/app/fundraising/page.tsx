0eaa81e9954e6a995353d000cd110f5fd914fb5f
Holeekey <danbrunosv@gmail.com>
2025-03-10 23:32:30 -0400

    feat(fundraisings): :sparkles: many fundraisings connected to blockchain

--- packages/nextjs/app/fundraising/page.tsx
+++ packages/nextjs/app/fundraising/page.tsx
@@ -2,20 +2,23 @@
 
 import { useRouter } from "next/navigation";
 import { fundraisingsData } from "./data/fundraisings.data";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 const FundraisingsMainPage: React.FC = () => {
   const router = useRouter();
+
+  const { data } = useScaffoldReadContract({ contractName: "CreateFunding", functionName: "retrieveAllFundings" });
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl font-bold text-center mb-8">Recaudaciones</h1>
       <p className="text-2xl text-center mb-8">Dona a las causas</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {fundraisingsData.map(fundraising => (
+        {data?.map(fundraising => (
           <div key={fundraising.id} className="bg-base-100 shadow-md rounded-lg p-6 flex flex-col justify-between">
             <div>
               <h2 className="text-2xl font-bold mb-2">{fundraising.title}</h2>
-              <p className="text-white-700 mb-4">{fundraising.description}</p>
+              <p className="text-white-700 mb-4">{fundraising.longDescription}</p>
             </div>
             <div>
               <div className="mb-4">
@@ -23,16 +26,20 @@ const FundraisingsMainPage: React.FC = () => {
                   <div className="flex mb-2 items-center justify-between">
                     <div>
                       <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200 text-[10px]">
-                        Recaudado: {fundraising.current} ETH
+                        Recaudado: {fundraising.totalEarned.toString()} ETH
                       </span>
                     </div>
                     <div className="text-right">
-                      <span className="text-xs font-semibold inline-block text-blue-600">{fundraising.goal} ETH</span>
+                      <span className="text-xs font-semibold inline-block text-blue-600">
+                        {fundraising.goal.toString()} ETH
+                      </span>
                     </div>
                   </div>
                   <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
                     <div
-                      style={{ width: `${(fundraising.current / fundraising.goal) * 100}%` }}
+                      style={{
+                        width: `${(parseInt(fundraising.totalEarned.toString()) || 0 / parseInt(fundraising.goal.toString()) || 1) * 100}%`,
+                      }}
                       className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"
                     ></div>
                   </div>
